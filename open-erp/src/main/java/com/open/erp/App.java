0eabdfc21e38476f4f084e235bae3855a2d4e79a
Thomas Florin <thoflo002@gmail.com>
2016-01-25 18:53:22 -0600

    example is now using Spark, and added compile time directories to gitignore
    Signed-off-by: Thomas Florin <thoflo002@gmail.com>

--- open-erp/src/main/java/com/open/erp/App.java
+++ open-erp/src/main/java/com/open/erp/App.java
@@ -1,5 +1,5 @@
 package com.open.erp;
-
+import static spark.Spark.*;
 /**
  * Hello world!
  *
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        get("/hello",(req,res)-> "Hello");
     }
 }
